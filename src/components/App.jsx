374593e811d1119251ec83920a47e3af7e2c117a
vivianwanjiku-lab <vivian.wanjiku@student.moringaschool.com>
2026-04-29 12:31:29 +0300

    Refactor App component to use child components
    Refactor App component to import and use Header, About, and ArticleList components, passing blog data as props.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,23 @@
-import React from "react";
-import blogData from "../data/blog";
-
-console.log(blogData);
+// App.jsx - Main component that orchestrates all other components
+// This component imports blog data and passes it down as props to child components
+import React from 'react';
+import Header from './components/Header';
+import About from './components/About';
+import ArticleList from './components/ArticleList';
+import blogData from './blog';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      {/* Pass the blog name to Header component */}
+      <Header name={blogData.name} />
+      
+      {/* Pass the blog image and about text to About component */}
+      <About image={blogData.image} about={blogData.about} />
+      
+      {/* Pass the blog posts array to ArticleList component */}
+      <ArticleList posts={blogData.posts} />
     </div>
   );
 }
